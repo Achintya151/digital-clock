a72730a1b4c62a5827ab84b786b5ce8813e6865d
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-11 16:10:08 +0600

    time displayed

--- js/app.js
+++ js/app.js
@@ -2,5 +2,28 @@
 const hours = document.getElementById('hr');
 const minutes = document.getElementById('min');
 const seconds = document.getElementById('sec');
-const amPm = document.getElementById('am-pm');
-console.log(hours, minutes, seconds, amPm);
+const amPmSpan = document.getElementById('am-pm');
+
+// functions
+
+const changeTime = () => {
+    const date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    let amPm = hour >= 12 ? 'PM' : 'AM';
+    hour = hour % 12;
+    // console.log(20 % 12);
+    hour = hour ? hour : 12;
+    hour = hour < 10 ? '0' + hour : hour;
+    minute = minute < 10 ? '0' + minute : minute;
+
+    // console.log(hour, minute, second);
+    hours.textContent = hour;
+    minutes.textContent = minute;
+    seconds.textContent = second;
+    amPmSpan.textContent = amPm;
+}
+changeTime();
+
