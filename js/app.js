398c61b4f4db90def793698faeacabd1641c1db5
Achintya151 <achintyakumartalukdar@gmail.com>
2022-09-11 15:42:01 +0600

    dom element added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// DOM Elements
+const hours = document.getElementById('hr');
+const minutes = document.getElementById('min');
+const seconds = document.getElementById('sec');
+const amPm = document.getElementById('am-pm');
+console.log(hours, minutes, seconds, amPm);
